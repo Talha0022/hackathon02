3245c73d10308007400a8aa13a9f7bbe9294e1bb
Talha Tariq <talhatariq2200@gmail.com>
2024-12-16 15:36:17 +0500

    2nd commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,59 +1,145 @@
+// import React from 'react';
+// import Link from 'next/link';
+// import styles from './footer.module.css';
+
+
+// export default function Footer () {
+//     return (
+//         <footer className = "footer">
+//           <div className = "footer-container">
+//             <div className = "footer-column">
+//               <ul>  
+//                 <Link href = "/" > <li> FIND A STORE      </li> </Link>
+//                 <Link href = "/" > <li> BECOME A MEMBER   </li> </Link>
+//                 <Link href = "/" > <li> SIGN UP FOR EMAIL </li> </Link>
+//                 <Link href = "/" > <li> Send Us Feedback  </li> </Link>
+//                 <Link href = "/" > <li> STUDENT DISCOUNTS </li> </Link>
+//               </ul>
+//             </div>
+//             <div className = "footer-column">
+//               <h4> GET HELP </h4> 
+//               <ul className="footer-column-ul" >
+//                 <Link href = "/" > <li> Order Status                      </li> </Link>
+//                 <Link href = "/" > <li> Delivery                          </li> </Link>
+//                 <Link href = "/" > <li> Returns                           </li> </Link>
+//                 <Link href = "/" > <li> Payment Options                   </li> </Link>
+//                 <Link href = "/" > <li> Contact Us On Nike.com Inquiries  </li> </Link>
+//                 <Link href = "/" > <li> Contact Us On All Other Inquiries </li> </Link>
+//               </ul>
+//             </div>
+//             <div className="footer-column">
+//               <h4>ABOUT NIKE</h4>
+//               <ul className="footer-column-ul" >
+//                 <Link href = "/" > <li> News           </li> </Link>
+//                 <Link href = "/" > <li> Careers        </li> </Link>
+//                 <Link href = "/" > <li> Investors      </li> </Link>
+//                 <Link href = "/" > <li> Sustainability </li> </Link>
+//               </ul>
+//             </div>
+//           </div>
+//           <div className="footer-bottom">
+//             <p>India </p>
+//             <p className="footer-column-ul" > &copy;  2023 Nike, Inc. All Rights Reserved </p>
+//             <ul className="footer-links">
+//               <Link href = "/" > <li> Guides              </li> </Link>
+//               <Link href = "/" > <li> Terms of Sale       </li> </Link>
+//               <Link href = "/" > <li> Terms of Use        </li> </Link>
+//               <Link href = "/" > <li> Nike Privacy Policy </li> </Link>
+//             </ul>
+//             <div className="footer-icons">
+//               <i className="fab fa-twitter"></i>
+//               <i className="fab fa-facebook"></i>
+//               <i className="fab fa-youtube"></i>
+//               <i className="fab fa-instagram"></i>
+//             </div>
+//           </div>
+//         </footer>     
+//   );
+// };
+
+
 import React from 'react';
 import Link from 'next/link';
+import styles from './footer.module.css';
 
-
-export default function Footer () {
-    return (
-        <footer className = "footer">
-          <div className = "footer-container">
-            <div className = "footer-column">
-              <ul>  
-                <Link href = "/" > <li> FIND A STORE      </li> </Link>
-                <Link href = "/" > <li> BECOME A MEMBER   </li> </Link>
-                <Link href = "/" > <li> SIGN UP FOR EMAIL </li> </Link>
-                <Link href = "/" > <li> Send Us Feedback  </li> </Link>
-                <Link href = "/" > <li> STUDENT DISCOUNTS </li> </Link>
-              </ul>
-            </div>
-            <div className = "footer-column">
-              <h4> GET HELP </h4> 
-              <ul className="footer-column-ul" >
-                <Link href = "/" > <li> Order Status                      </li> </Link>
-                <Link href = "/" > <li> Delivery                          </li> </Link>
-                <Link href = "/" > <li> Returns                           </li> </Link>
-                <Link href = "/" > <li> Payment Options                   </li> </Link>
-                <Link href = "/" > <li> Contact Us On Nike.com Inquiries  </li> </Link>
-                <Link href = "/" > <li> Contact Us On All Other Inquiries </li> </Link>
-              </ul>
-            </div>
-            <div className="footer-column">
-              <h4>ABOUT NIKE</h4>
-              <ul className="footer-column-ul" >
-                <Link href = "/" > <li> News           </li> </Link>
-                <Link href = "/" > <li> Careers        </li> </Link>
-                <Link href = "/" > <li> Investors      </li> </Link>
-                <Link href = "/" > <li> Sustainability </li> </Link>
-              </ul>
-            </div>
-          </div>
-          <div className="footer-bottom">
-            <p>India </p>
-            <p className="footer-column-ul" > &copy;  2023 Nike, Inc. All Rights Reserved </p>
-            <ul className="footer-links">
-              <Link href = "/" > <li> Guides              </li> </Link>
-              <Link href = "/" > <li> Terms of Sale       </li> </Link>
-              <Link href = "/" > <li> Terms of Use        </li> </Link>
-              <Link href = "/" > <li> Nike Privacy Policy </li> </Link>
-            </ul>
-            <div className="footer-icons">
-              <i className="fab fa-twitter"></i>
-              <i className="fab fa-facebook"></i>
-              <i className="fab fa-youtube"></i>
-              <i className="fab fa-instagram"></i>
-            </div>
-          </div>
-        </footer>     
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles['footer-container']}>
+        <div className={styles['footer-column']}>
+          <ul>
+             <li> <Link href="/"> FIND A STORE      </Link> </li>      
+             <li> <Link href="/"> BECOME A MEMBER   </Link> </li>   
+             <li> <Link href="/"> SIGN UP FOR EMAIL </Link> </li> 
+             <li> <Link href="/"> Send Us Feedback  </Link> </li>  
+             <li> <Link href="/"> STUDENT DISCOUNTS </Link> </li> 
+          </ul>
+        </div>
+        <div className={styles['footer-column']}>
+          <h4>GET HELP</h4>
+          <ul className={styles['footer-column-ul']}>
+            <Link href="/">
+              <li>Order Status</li>
+            </Link>
+            <Link href="/">
+              <li>Delivery</li>
+            </Link>
+            <Link href="/">
+              <li>Returns</li>
+            </Link>
+            <Link href="/">
+              <li>Payment Options</li>
+            </Link>
+            <Link href="/">
+              <li>Contact Us On Nike.com Inquiries</li>
+            </Link>
+            <Link href="/">
+              <li>Contact Us On All Other Inquiries</li>
+            </Link>
+          </ul>
+        </div>
+        <div className={styles['footer-column']}>
+          <h4>ABOUT NIKE</h4>
+          <ul className={styles['footer-column-ul']}>
+            <Link href="/">
+              <li>News</li>
+            </Link>
+            <Link href="/">
+              <li>Careers</li>
+            </Link>
+            <Link href="/">
+              <li>Investors</li>
+            </Link>
+            <Link href="/">
+              <li>Sustainability</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+      <div className={styles['footer-bottom']}>
+        <p>India</p>
+        <p className={styles['footer-column-ul']}>&copy; 2023 Nike, Inc. All Rights Reserved</p>
+        <ul className={styles['footer-links']}>
+          <Link href="/">
+            <li>Guides</li>
+          </Link>
+          <Link href="/">
+            <li>Terms of Sale</li>
+          </Link>
+          <Link href="/">
+            <li>Terms of Use</li>
+          </Link>
+          <Link href="/">
+            <li>Nike Privacy Policy</li>
+          </Link>
+        </ul>
+        <div className={styles['footer-icons']}>
+          <i className="fab fa-twitter"></i>
+          <i className="fab fa-facebook"></i>
+          <i className="fab fa-youtube"></i>
+          <i className="fab fa-instagram"></i>
+        </div>
+      </div>
+    </footer>
   );
-};
-
-
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,157 @@
-import React  from "react";
-import Image  from "next/image";
-import Link   from "next/link";
+// import React  from "react";
+// import Image  from "next/image";
+// import Link   from "next/link";
+// import Frame1 from "@/app/public/Frame1.png";
+// import { FiHeart } from "react-icons/fi";
+// import { IoBagOutline } from "react-icons/io5";
+// import { IoIosSearch } from "react-icons/io";
+
+
+// export default function Header() {
+//     return (
+//         <>
+//             <header className = "header-container">
+//                 {/* Top Links */}
+//                 <div className = "top-links">
+//                     <ul className = "top-links-list">
+//                         <Link href = "/find-a-store"> <li> Find a Store </li> </Link>
+//                         <Link href = "/help        "> <li> Help         </li> </Link>
+//                         <Link href = "/join-us     "> <li> Join Us      </li> </Link>
+//                         <Link href = "/sign-in     "> <li> Sign In      </li> </Link>
+//                     </ul>
+//                 </div>
+
+//                 {/* Main Header */}
+//                 <div className = "main-header">
+//                     {/* Logo */}
+//                     <div className = "logo-container">
+//                         <Image
+//                             className = "logo"
+//                             src       = {Frame1}
+//                             alt       = "Nike Logo"
+//                             layout    = "intrinsic" 
+//                             width     = {80} 
+//                             height    = {80} 
+//                         />
+//                     </div>
+
+//                     {/* Center: Navigation Menu */}
+//                     <nav className = "nav-menu">
+//                         <ul className = "menu-list">
+//                             <Link href = "/New&Featured "> <li> New & Featured </li> </Link>
+//                             <Link href = "/Men          "> <li> Men            </li> </Link>
+//                             <Link href = "/Women        "> <li> Women          </li> </Link>
+//                             <Link href = "/Kids         "> <li> Kids           </li> </Link>
+//                             <Link href = "/Sale         "> <li> Sale           </li> </Link>
+//                             <Link href = "/SNKRS        "> <li> SNKRS          </li> </Link>
+//                         </ul>
+//                     </nav>
+
+//                     {/* Right: Search Bar and Icons */}
+//                     <div className = "header-icons">
+//                         <div className = "search-bar">
+//                         <IoIosSearch className="search-icon" />
+//                             <input type = "text" 
+//                                    placeholder = "Search" 
+//                                    className = "search-input" />
+//                         </div>
+//                         <button className = "icon-btn"> <FiHeart className="tow-icons" />        </button>
+//                         <button className = "icon-btn"> <IoBagOutline className="tow-icons" />   </button>
+//                     </div>
+//                 </div>
+//             </header>
+//         </>
+//     );
+// }
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import Frame1 from "@/app/public/Frame1.png";
+import { FiHeart } from "react-icons/fi";
+import { IoBagOutline } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
+import styles from "./header.module.css"; // Import the CSS module
 
 export default function Header() {
-    return (
-        <>
-            <header className = "header-container">
-                {/* Top Links */}
-                <div className = "top-links">
-                    <ul className = "top-links-list">
-                        <Link href = "/find-a-store"> <li> Find a Store </li> </Link>
-                        <Link href = "/help        "> <li> Help         </li> </Link>
-                        <Link href = "/join-us     "> <li> Join Us      </li> </Link>
-                        <Link href = "/sign-in     "> <li> Sign In      </li> </Link>
-                    </ul>
-                </div>
+  return (
+    <>
+      <header className={styles["header-container"]}> {/* Use CSS module class */}
+        {/* Top Links */}
+        <div className={styles["top-links"]}>
+          <ul className={styles["top-links-list"]}>
+            <Link href="/find-a-store">
+              <li>Find a Store</li>
+            </Link>
+            <Link href="/help">
+              <li>Help</li>
+            </Link>
+            <Link href="/join-us">
+              <li>Join Us</li>
+            </Link>
+            <Link href="/sign-in">
+              <li>Sign In</li>
+            </Link>
+          </ul>
+        </div>
 
-                {/* Main Header */}
-                <div className = "main-header">
-                    {/* Logo */}
-                    <div className = "logo-container">
-                        <Image
-                            className = "logo"
-                            src       = {Frame1}
-                            alt       = "Nike Logo"
-                            layout    = "intrinsic" 
-                            width     = {80} 
-                            height    = {80} 
-                        />
-                    </div>
+        {/* Main Header */}
+        <div className={styles["main-header"]}>
+          {/* Logo */}
+          <div className={styles["logo-container"]}>
+            <Image
+              className={styles.logo} // Use CSS module class for logo
+              src={Frame1}
+              alt="Nike Logo"
+              layout="intrinsic"
+              width={80}
+              height={80}
+            />
+          </div>
 
-                    {/* Center: Navigation Menu */}
-                    <nav className = "nav-menu">
-                        <ul className = "menu-list">
-                            <Link href = "/New&Featured "> <li> New & Featured </li> </Link>
-                            <Link href = "/Men          "> <li> Men            </li> </Link>
-                            <Link href = "/Women        "> <li> Women          </li> </Link>
-                            <Link href = "/Kids         "> <li> Kids           </li> </Link>
-                            <Link href = "/Sale         "> <li> Sale           </li> </Link>
-                            <Link href = "/SNKRS        "> <li> SNKRS          </li> </Link>
-                        </ul>
-                    </nav>
+          {/* Center: Navigation Menu */}
+          <nav className={styles["nav-menu"]}>
+            <ul className={styles["menu-list"]}>
+              <Link href="/New&Featured">
+                <li>New & Featured</li>
+              </Link>
+              <Link href="/Men">
+                <li>Men</li>
+              </Link>
+              <Link href="/Women">
+                <li>Women</li>
+              </Link>
+              <Link href="/Kids">
+                <li>Kids</li>
+              </Link>
+              <Link href="/Sale">
+                <li>Sale</li>
+              </Link>
+              <Link href="/SNKRS">
+                <li>SNKRS</li>
+              </Link>
+            </ul>
+          </nav>
 
-                    {/* Right: Search Bar and Icons */}
-                    <div className = "header-icons">
-                        <div className = "search-bar">
-                            <input type = "text" 
-                                   placeholder = "Search" 
-                                   className = "search-input" />
-                        </div>
-                        <button className = "icon-btn"> Favourite </button>
-                        <button className = "icon-btn"> Cart      </button>
-                    </div>
-                </div>
-            </header>
-        </>
-    );
+          {/* Right: Search Bar and Icons */}
+          <div className={styles["header-icons"]}>
+            <div className={styles["search-bar"]}>
+              <IoIosSearch className={styles["search-icon"]} />
+              <input
+                type="text"
+                placeholder="Search"
+                className={styles["search-input"]}
+              />
+            </div>
+            <button className={styles["icon-btn"]}>
+              <FiHeart className={styles["tow-icons"]} />
+            </button>
+            <button className={styles["icon-btn"]}>
+              <IoBagOutline className={styles["tow-icons"]} />
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
