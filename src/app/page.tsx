3a212911ef6219b561344db7bf7055446ac2da67
Talha Tariq <talhatariq2200@gmail.com>
2024-12-17 00:04:22 +0500

    3rd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-// import Image from "next/image";
+import Image from "next/image";
+import nike from "../../public/nike.png";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Home page
-    
+    <div className="airmax-container">
+      {/* Header Section */}
+      <div className="header">
+        <h1 className="title1">Hello Nike App</h1>
+        <p className="text1">
+          Download the app to access everything Nike.{" "}
+          <Link href="./">
+            <u>Get Your Great</u>
+          </Link>
+        </p>
+      </div>
+
+      {/* Image Section */}
+      <div className="image-section">
+        <Image
+          src={nike}
+          alt="Nike Air Max Pulse"
+          layout="responsive"
+          width={1344}
+          height={700}
+          priority
+        />
+      </div>
+
+      {/* Text Content Section */}
+      <div className="text-section">
+        <h1 className="title">NIKE AIR MAX PULSE</h1>
+        <p className="description">
+          Extreme comfort. Hyper durable. Max volume. Introducing the Air Max Pulse —
+          designed to push you past your limits and help you go to the max.
+        </p>
+        <div className="buttons">
+          <button className="button notify">Notify Me</button>
+          <button className="button shop">Shop Air Max</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,73 +1,18 @@
-// import React from 'react';
-// import Link from 'next/link';
-// import styles from './footer.module.css';
-
-
-// export default function Footer () {
-//     return (
-//         <footer className = "footer">
-//           <div className = "footer-container">
-//             <div className = "footer-column">
-//               <ul>  
-//                 <Link href = "/" > <li> FIND A STORE      </li> </Link>
-//                 <Link href = "/" > <li> BECOME A MEMBER   </li> </Link>
-//                 <Link href = "/" > <li> SIGN UP FOR EMAIL </li> </Link>
-//                 <Link href = "/" > <li> Send Us Feedback  </li> </Link>
-//                 <Link href = "/" > <li> STUDENT DISCOUNTS </li> </Link>
-//               </ul>
-//             </div>
-//             <div className = "footer-column">
-//               <h4> GET HELP </h4> 
-//               <ul className="footer-column-ul" >
-//                 <Link href = "/" > <li> Order Status                      </li> </Link>
-//                 <Link href = "/" > <li> Delivery                          </li> </Link>
-//                 <Link href = "/" > <li> Returns                           </li> </Link>
-//                 <Link href = "/" > <li> Payment Options                   </li> </Link>
-//                 <Link href = "/" > <li> Contact Us On Nike.com Inquiries  </li> </Link>
-//                 <Link href = "/" > <li> Contact Us On All Other Inquiries </li> </Link>
-//               </ul>
-//             </div>
-//             <div className="footer-column">
-//               <h4>ABOUT NIKE</h4>
-//               <ul className="footer-column-ul" >
-//                 <Link href = "/" > <li> News           </li> </Link>
-//                 <Link href = "/" > <li> Careers        </li> </Link>
-//                 <Link href = "/" > <li> Investors      </li> </Link>
-//                 <Link href = "/" > <li> Sustainability </li> </Link>
-//               </ul>
-//             </div>
-//           </div>
-//           <div className="footer-bottom">
-//             <p>India </p>
-//             <p className="footer-column-ul" > &copy;  2023 Nike, Inc. All Rights Reserved </p>
-//             <ul className="footer-links">
-//               <Link href = "/" > <li> Guides              </li> </Link>
-//               <Link href = "/" > <li> Terms of Sale       </li> </Link>
-//               <Link href = "/" > <li> Terms of Use        </li> </Link>
-//               <Link href = "/" > <li> Nike Privacy Policy </li> </Link>
-//             </ul>
-//             <div className="footer-icons">
-//               <i className="fab fa-twitter"></i>
-//               <i className="fab fa-facebook"></i>
-//               <i className="fab fa-youtube"></i>
-//               <i className="fab fa-instagram"></i>
-//             </div>
-//           </div>
-//         </footer>     
-//   );
-// };
-
-
 import React from 'react';
 import Link from 'next/link';
 import styles from './footer.module.css';
+import { BsFacebook } from "react-icons/bs";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { TiSocialYoutubeCircular } from "react-icons/ti";
+import { ImInstagram } from "react-icons/im";
+import { FaLocationDot } from "react-icons/fa6";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles['footer-container']}>
         <div className={styles['footer-column']}>
-          <ul>
+          <ul className={styles['footer-column-ul']}>
              <li> <Link href="/"> FIND A STORE      </Link> </li>      
              <li> <Link href="/"> BECOME A MEMBER   </Link> </li>   
              <li> <Link href="/"> SIGN UP FOR EMAIL </Link> </li> 
@@ -78,66 +23,48 @@ export default function Footer() {
         <div className={styles['footer-column']}>
           <h4>GET HELP</h4>
           <ul className={styles['footer-column-ul']}>
-            <Link href="/">
-              <li>Order Status</li>
-            </Link>
-            <Link href="/">
-              <li>Delivery</li>
-            </Link>
-            <Link href="/">
-              <li>Returns</li>
-            </Link>
-            <Link href="/">
-              <li>Payment Options</li>
-            </Link>
-            <Link href="/">
-              <li>Contact Us On Nike.com Inquiries</li>
-            </Link>
-            <Link href="/">
-              <li>Contact Us On All Other Inquiries</li>
-            </Link>
+            <Link href="/"><li>Order Status</li></Link>
+            <Link href="/"><li>Delivery</li></Link>
+            <Link href="/"><li>Returns</li></Link>
+            <Link href="/"><li>Payment Options</li></Link>
+            <Link href="/"><li>Contact Us On Nike.com Inquiries</li></Link>
+            <Link href="/"><li>Contact Us On All Other Inquiries</li></Link>
           </ul>
         </div>
-        <div className={styles['footer-column']}>
+        <div className={styles['footer-column']}>              
           <h4>ABOUT NIKE</h4>
           <ul className={styles['footer-column-ul']}>
-            <Link href="/">
-              <li>News</li>
-            </Link>
-            <Link href="/">
-              <li>Careers</li>
-            </Link>
-            <Link href="/">
-              <li>Investors</li>
-            </Link>
-            <Link href="/">
-              <li>Sustainability</li>
-            </Link>
+            <Link href="/"><li>News</li></Link>
+            <Link href="/"><li>Careers</li></Link>
+            <Link href="/"><li>Investors</li></Link>
+            <Link href="/"><li>Sustainability</li></Link>
           </ul>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div className="grid grid-flow-col gap-4">
+            <AiFillTwitterCircle     className="w-[40px] h-[40px] fill-#7e7e7e hover:fill-blue-700"/>
+            <BsFacebook              className="w-[34px] h-[34px] fill-#7e7e7e hover:fill-blue-700"/>
+            <TiSocialYoutubeCircular className="w-[45px] h-[45px] fill-#7e7e7e hover:fill-blue-700"/>
+            <ImInstagram             className="w-[29px] h-[29px] fill-#7e7e7e hover:fill-blue-700"/>
         </div>
       </div>
       <div className={styles['footer-bottom']}>
-        <p>India</p>
-        <p className={styles['footer-column-ul']}>&copy; 2023 Nike, Inc. All Rights Reserved</p>
-        <ul className={styles['footer-links']}>
-          <Link href="/">
-            <li>Guides</li>
-          </Link>
-          <Link href="/">
-            <li>Terms of Sale</li>
-          </Link>
-          <Link href="/">
-            <li>Terms of Use</li>
-          </Link>
-          <Link href="/">
-            <li>Nike Privacy Policy</li>
-          </Link>
-        </ul>
-        <div className={styles['footer-icons']}>
-          <i className="fab fa-twitter"></i>
-          <i className="fab fa-facebook"></i>
-          <i className="fab fa-youtube"></i>
-          <i className="fab fa-instagram"></i>
+        <div className={styles['footer-bottom-1']}>  
+          <FaLocationDot />
+          <p>Pakistan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &copy;  2023 Nike, Inc. All Rights Reserved</p>
+        </div>
+        <div>
+          <ul className={styles['footer-links']}>
+            <Link href="/"><li>Guides</li></Link>
+            <Link href="/"><li>Terms of Sale</li></Link>
+            <Link href="/"><li>Terms of Use</li></Link>
+            <Link href="/"><li>Nike Privacy Policy</li></Link>
+          </ul>
         </div>
       </div>
     </footer>
